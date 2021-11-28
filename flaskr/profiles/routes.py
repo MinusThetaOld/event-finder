366638e885ab2a0b3d0bfeec861ba22dacc7c097
JanayAlam <janayalam820@gmail.com>
2021-11-28 19:43:39 +0600

    feat: Created html files for account settings

--- flaskr/profiles/routes.py
+++ flaskr/profiles/routes.py
@@ -8,3 +8,23 @@ profiles = Blueprint("profiles", __name__)
 def view_profile(id: int):
     user = User.query.get(id)
     return render_template("profiles/view-profile.html", user=user)
+
+
+@profiles.route("/profiles/change-info")
+def change_profile_info():
+    return render_template("profiles/edit-profile-info.html", active="edit-profile-info")
+
+
+@profiles.route("/profiles/change-photos")
+def change_photos():
+    return render_template("profiles/change-photos.html", active="change-photos")
+
+
+@profiles.route("/profiles/verify-email")
+def verify_email():
+    return render_template("profiles/verify-email.html", active="verify-email")
+
+
+@profiles.route("/profiles/change-password")
+def change_password():
+    return render_template("profiles/change-password.html", active="change-password")
