38cd5b225ef50c1245fdd6e4a854c76482b9939c
JanayAlam <janayalam820@gmail.com>
2021-11-29 02:45:37 +0600

    formate: Changes some html code

--- flaskr/profiles/forms.py
+++ flaskr/profiles/forms.py
@@ -1,17 +1,15 @@
-from flask_migrate import current
-from flask_wtf import FlaskForm
-from flaskr.models import Profile, User
-from wtforms import (BooleanField, DateField, EmailField, PasswordField,
-                     SelectField, StringField, SubmitField, TextAreaField)
-from wtforms.validators import (DataRequired, Email, EqualTo, Length,
-                                ValidationError)
 from flask_login import current_user
+from flask_wtf import FlaskForm
 from flaskr import bcrypt
+from flaskr.models import User
+from wtforms import (DateField, PasswordField, StringField, SubmitField,
+                     TextAreaField)
+from wtforms.validators import DataRequired, EqualTo, Length, ValidationError
 
 
-# Ashiq
 class ProfileInfoForm(FlaskForm):
-    bio =  TextAreaField("Bio", validators=[Length(max=500)],  render_kw={"placeholder": "Write your bio here..."})
+    bio = TextAreaField("Bio", validators=[Length(max=500)],  render_kw={
+                        "placeholder": "Write your bio here..."})
     first_name = StringField("First Name", validators=[
         DataRequired(), Length(max=15, min=2)
     ], render_kw={"placeholder": "ex. Alen"})
@@ -23,34 +21,35 @@ class ProfileInfoForm(FlaskForm):
     ])
     save = SubmitField("Save")
 
-# Alam
+
 class VerifyEmailForm(FlaskForm):
     token = StringField("Verification Token", validators=[
         DataRequired(), Length(max=6)
-    ], render_kw={ "placeholder": "Enter your verification token here..." })
+    ], render_kw={"placeholder": "Enter your verification token here..."})
     save = SubmitField("Save")
 
-# Shamsur
+
 class ChangePasswordForm(FlaskForm):
     old_password = PasswordField("Old Password", validators=[
         DataRequired(), Length(min=6)
     ], render_kw={"placeholder": "Type your old password"})
-    
+
     new_password = PasswordField("New Password", validators=[
         DataRequired(), Length(min=6)
     ], render_kw={"placeholder": "Type a new password"})
-    
+
     c_password = PasswordField("Confirm Password", validators=[
         DataRequired(), Length(min=6), EqualTo(
             "new_password", "Confirm password did not matched")
     ], render_kw={"placeholder": "Retype the password"})
-    
+
     submit = SubmitField("Save")
-    
+
     def validate_old_password(self, odl_password):
         user = User.query.get(current_user.id)
         if not bcrypt.check_password_hash(user.password, odl_password.data):
             raise ValidationError("Password did not matched.")
+
 
 class ChangePhoto():
     pass
