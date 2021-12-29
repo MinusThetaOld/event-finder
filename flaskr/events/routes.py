ac544f9df2efffc47a5e657fd7c7e7b3181bcb74
shamsur rahman <shamsur.works@gmail.com>
2021-12-29 19:29:05 +0600

    feat: Implemented event info form

--- flaskr/events/routes.py
+++ flaskr/events/routes.py
@@ -4,7 +4,7 @@ from flaskr import db
 from flaskr.decorators import is_host
 from flaskr.events.forms import *
 from flaskr.models import Event, Notification, Profile, Role, User
-from flaskr.profiles.utils import save_photos
+from flaskr.profiles.utils import save_photos, remove_photo
 from sqlalchemy import desc
 
 events = Blueprint("events", __name__)
@@ -52,10 +52,39 @@ def event_info(id: int):
         flash("Only the host can access this route", "danger")
         return redirect(url_for("events.view_event", id=event.id))
     form = EventForm()
-    if form.validate_on_submit():
+    if request.method == "POST":
         # post request
-        pass
+        event.title = form.event_title.data
+        event.description = form.event_description.data
+        event.place_name = form.event_location.data
+        event.event_time = form.event_start_time.data
+        event.day = form.event_days_count.data
+        event.night = form.event_nights_count.data
+        event.fee = form.event_fee.data
+        event.cover_photo = form.event_cover_photo.data
+        event.hotel_name = form.hotel_name.data
+        event.hotel_weblink = form.hotel_web_link.data
+        if form.event_cover_photo.data:
+            file_path = event.cover_photo
+            if not ("/images/default/CoverPhotos/event-default.png" in file_path):
+                remove_photo(file_path)
+            # saving
+            photo_file = save_photos(
+                form.event_cover_photo.data, current_user.id, "eventCover", 1040, 260)
+            event.cover_photo = "/images/uploads/eventCover/" + photo_file
+        db.session.commit()
+        flash(f"Event information saved", "success")
     # add the values
+    elif request.method == "GET":
+        form.event_title.data = event.title
+        form.event_description.data = event.description
+        form.event_location.data = event.place_name
+        form.event_start_time.data = event.event_time
+        form.event_days_count.data = event.day
+        form.event_nights_count.data = event.night
+        form.event_fee.data = event.fee
+        form.hotel_name.data = event.hotel_name
+        form.hotel_web_link.data = event.hotel_weblink
     return render_template("events/event-info.html", active="event-info", form=form, event=event)
 
 
